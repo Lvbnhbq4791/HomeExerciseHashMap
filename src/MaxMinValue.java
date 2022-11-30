323749e17a04352a28cee33210ed599cc869c8ff
Dmitriy Ogorodnikov <diminskiy_4791@mail.ru>
2022-11-30 13:44:22 +0300

    Коммит 1

---
+++ src/MaxMinValue.java
@@ -0,0 +1,69 @@
+import java.util.Map;
+
+public class MaxMinValue {
+    protected Map<Character, Integer> symbolCollection;
+    protected int maxValue;
+    protected int minValue;
+
+    public MaxMinValue(Map<Character, Integer> symbolCollection) {
+        this.symbolCollection = symbolCollection;
+        this.maxValue = -1;
+        this.minValue = Integer.MAX_VALUE;
+    }
+
+    // метод определения максимального количества повторений
+    public void setMaxValue() {
+        for (Map.Entry<Character, Integer> keyValue : symbolCollection.entrySet()) {
+            if (keyValue.getValue() > maxValue) {
+                maxValue = keyValue.getValue();
+            }
+        }
+    }
+
+    // метод определения минимального количества повторений
+    public void setMinValue() {
+        for (Map.Entry<Character, Integer> keyValue : symbolCollection.entrySet()) {
+            if (keyValue.getValue() < minValue) {
+                minValue = keyValue.getValue();
+            }
+        }
+    }
+
+    // метод вывода символов с максимальным количеством повторений
+    public String maxNumberOfSymbol() {
+        String text;
+        int k = 0;
+        StringBuilder sBtext = new StringBuilder();
+        for (Map.Entry<Character, Integer> keyValue : symbolCollection.entrySet()) {
+            if (keyValue.getValue().equals(maxValue)) {
+                if (k > 0) {
+                    sBtext.append("\n");
+                }
+                k++;
+                String t = (k + ". " + "Символ " + "('" + keyValue.getKey() + "')" + " колличество повторений " + keyValue.getValue());
+                sBtext.append(t);
+            }
+        }
+        text = sBtext.toString();
+        return text;
+    }
+
+    // метод вывода символов с минимальным количеством повторений
+    public String minNumberOfSymbol() {
+        String text;
+        int k = 0;
+        StringBuilder sBtext = new StringBuilder();
+        for (Map.Entry<Character, Integer> keyValue : symbolCollection.entrySet()) {
+            if (keyValue.getValue().equals(minValue)) {
+                if (k > 0) {
+                    sBtext.append("\n");
+                }
+                k++;
+                String t = (k + ". " + "Символ " + "('" + keyValue.getKey() + "')" + " колличество повторений " + keyValue.getValue());
+                sBtext.append(t);
+            }
+        }
+        text = sBtext.toString();
+        return text;
+    }
+}
